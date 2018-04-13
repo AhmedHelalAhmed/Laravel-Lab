5f4e09a47f8218beb05a082479696f63bce7bffb
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-04-13 19:46:40 +0200

    add posts index view and send post model to view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        dd("hello from posts");
+        // dd("hello from posts");
+        // return Post::all();
+        $posts=Post::all();
+        // dd($posts);
+        /**
+         * posts the key can be access from the view
+         * $posts just variable contain array comes from database
+         */
+
+        /**
+         * posts.index
+         * posts is the directory in resources/views
+         * call method index
+         */
+        return view('posts.index',['posts'=>$posts]);
     }
 }
